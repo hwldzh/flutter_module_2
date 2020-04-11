ca96be3b92535c6cb14f8943a4d5ac4bf22d5ad4
hwldzh <821187649@qq.com>
2020-04-11 10:27:59 +0800

    feat: 修改名字

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
 import 'first.dart';
 
-void main() => (runApp(getWidgetFlutterModule2("", "")));
+void main() => (runApp(getWidgetFlutterModule("", "")));
 
-Widget getWidgetFlutterModule2(String pageName, String query) {
+Widget getWidgetFlutterModule(String pageName, String query) {
   if(pageName == "mainPage") {
     int index = query.indexOf("haveNav=1");
     if(index != -1) { //接收到参数，这里简单处理，直接跳转到有导航栏的界面
       return MyModule2();
-    } else {
-      return Center(
-        child: Text('Unknown pageName: $pageName', textDirection: TextDirection.ltr),
-      );
-    }
-  } else {
-    return Center(
-      child: Text('Unknown pageName: $pageName', textDirection: TextDirection.ltr),
-    );
+    } 
   }
+  return Center(
+    child: Text('Unknown pageName: $pageName', textDirection: TextDirection.ltr),
+  );
 }
